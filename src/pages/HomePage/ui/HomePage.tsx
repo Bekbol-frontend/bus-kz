7ac6d0b147ca824c5a81869f06d1fbd3e48182f8
Bekbol <bekbold@MacBook-Air-Bekbol.local>
2025-12-21 09:32:23 -0800

    change style main title home

--- src/pages/HomePage/ui/HomePage.tsx
+++ src/pages/HomePage/ui/HomePage.tsx
@@ -5,16 +5,18 @@ import styles from "./HomePage.module.scss";
 import { useTranslation } from "react-i18next";
 import SearchingTicket from "./SearchingTicket/SearchingTicket";
 import { StepsTicket } from "@/shared/ui/StepsTicket";
+import { useResponsive } from "@/shared/lib/hooks/useResponsive";
 
 const { Title } = Typography;
 
 function HomePage() {
   const { t } = useTranslation();
+  const { xs } = useResponsive();
 
   return (
     <Section>
       <Container>
-        <Title className={styles.title}>{t("Buy or book tickets")}</Title>
+        <Title level={ xs ? 4 : 2 } className={styles.title}>{t("Buy or book tickets")}</Title>
         <SearchingTicket />
         <StepsTicket />
       </Container>
